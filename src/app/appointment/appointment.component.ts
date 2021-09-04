095da59410b7841981a8c56eb1bb7d288c27d71e
Stefânio Jr <stefaniojr@live.com>
2021-09-04 14:34:11 -0300

    feat: inicializa o módulo appointment.

---
+++ src/app/appointment/appointment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment',
+  templateUrl: './appointment.component.html',
+  styleUrls: ['./appointment.component.scss']
+})
+export class AppointmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/appointment/appointment.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppointmentRoutingModule } from './appointment-routing.module';
+import { AppointmentComponent } from './appointment/appointment.component';
+
+
+@NgModule({
+  declarations: [
+    AppointmentComponent
+  ],
+  imports: [
+    CommonModule,
+    AppointmentRoutingModule
+  ]
+})
+export class AppointmentModule { }
