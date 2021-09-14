60dcd17a3cb4ac106894bf03ec6d13194482e759
Stefânio Jr <stefaniojr@live.com>
2021-09-14 03:04:30 -0300

    feat: implementa onboarding.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,25 +7,57 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
-    trigger('fade', [      
+    trigger('fade', [
       transition('void => *', [
-        style({opacity: 0}),
-        animate(1000, style({opacity: 1}))
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
       ]),
-      transition('* => void', [
-        animate(1000, style({opacity: 0}))
-      ])
-    ])]
+      transition('* => void', [animate(1000, style({ opacity: 0 }))]),
+    ]),
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('400ms', style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
+  onOnBoarding = true;
+  onBoarding1 = true;
+  onBoarding2 = false;
+  onBoarding3 = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goAppointment() {
     this.router.navigate(['/appointment']);
   }
 
+  goOnBoarding1() {
+    this.onBoarding1 = true;
+    this.onBoarding2 = false;
+    this.onBoarding3 = false;
+  }
+
+  goOnBoarding2() {
+    this.onBoarding1 = false;
+    this.onBoarding2 = true;
+    this.onBoarding3 = false;
+  }
+
+  goOnBoarding3() {
+    this.onBoarding1 = false;
+    this.onBoarding2 = false;
+    this.onBoarding3 = true;
+  }
+
+  dismissOnBoarding() {
+    this.onOnBoarding = false;
+    this.onBoarding1 = false;
+    this.onBoarding2 = false;
+    this.onBoarding3 = false;
+  }
 }
